41fe296dd6a2045de68dccf2fba3d55d659f0719
jsh <jsh@DESKTOP-9LU8KB0>
2019-04-30 18:54:30 +0900

    build change

---
+++ src/ll/Test1234.java
@@ -0,0 +1,15 @@
+package ll;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test1234 {
+
+	@Test
+	public void test() {
+		int a = 3;
+		System.out.println(a);
+	}
+
+}
---
+++ src/ll/testTest.java
@@ -0,0 +1,14 @@
+package ll;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testTest {
+
+	@Test
+	public void test() {
+		System.out.println("Junit test");
+	}
+
+}
